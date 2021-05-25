6f44626057d96b8db390f40ae4987f8d477a22ed
Rogerhz <rogerhernandez0260@gmail.com>
2021-05-25 00:41:56 +0000

    proceso

--- src/app.py
+++ src/app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify 
 
 app = Flask(__name__)
 
+todos = [
+    { "label": "My first task", "done": False }
+]
 
-# @app.route("myporfolio.com/home")
-# def home():
-#     return """
-#         <div></div>
-#     """
-# todolist = []
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = flask.jsonify()
+    return json_text
+
+# These two lines should always be at the end of your app.py file.
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
